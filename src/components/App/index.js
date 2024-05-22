93c34b8745c9ec974f0922af1ef2ce226a9915aa
Yirzaraji <not.iremy@gmail.com>
2024-05-22 10:55:37 +0200

    update #1

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import "../../App.css";
+import Footer from "../Footer";
+import Header from "../Header";
+import Landing from "../Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <p>Projet Yirzaraji 2024</p>
+      <p>Cette app utilise l'API Starwars de swapi</p>
+    </footer>
+  );
+};
+
+export default Footer;
